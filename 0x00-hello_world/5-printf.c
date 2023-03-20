f7788e74a4e8449ddae31c662fdaf6a05fc98f04
Laopan-sudo <laopanpare@proonmail.com>
2023-03-20 11:48:09 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a message to the console using the
+ *              puts() function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
