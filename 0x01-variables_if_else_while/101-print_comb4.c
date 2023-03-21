ea0b3ab536e8c1761472e58383ccad153e49616d
Laopan-sudo <laopanpare@proonmail.com>
2023-03-20 20:13:40 -0700

    The success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Main - Entry
- *Description: Prints
- * Return: Always 0 (Success)
+ * main - Entyr point
+ * Description: prints all possible different combinations of three digits
+ * Return: Always 0 (success)
  */
 
 int main(void)
