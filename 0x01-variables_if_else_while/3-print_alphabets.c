c3aaac984712a02cd7ed825671977bce2d7e9e05
Laopan-sudo <laopanpare@proonmail.com>
2023-03-20 18:53:31 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: alphabet
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char letter = 'a';
+
+	do {
+		putchar(letter);
+		letter++;
+	} while (letter <= 'z');
+
+	letter = 'A'
+
+	do {
+		putchar(letter);
+		letter++;
+	} while (letter <= 'Z');
+
+	putchar('\n');
+
+	return (0);
+}
+i
