6e36d9b91f23eafa470c0be449b8fb38a8d15914
Laopan-sudo <laopanpare@proonmail.com>
2023-03-22 16:52:11 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a new line
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times
  * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+	char letter;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
 }
