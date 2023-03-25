b82123a51d39dd5f6093b36a67448ca657ebdee6
Laopan-sudo <laopanpare@proonmail.com>
2023-03-25 08:24:59 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,15 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int i){
+	if (i >0){
+		printf("positive");
+	}
+	else if (i < 0){
+		printf("negative");
+	}
+	else
+		printf("zero");
+}
 
 #endif
