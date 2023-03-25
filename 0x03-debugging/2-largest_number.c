d2156ba1a9740bee0c6ccbb1773144e52fcc08c7
Laopan-sudo <laopanpare@proonmail.com>
2023-03-25 09:34:04 -0700

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,13 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest = 0;
 
-	if (a > b && a > c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c)
 	{
 		largest = b;
 	}
