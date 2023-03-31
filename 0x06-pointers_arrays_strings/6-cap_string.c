cf8ac0b0fde4af327a3c57777b7fc9b1be43e966
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 09:23:34 -0700

    Expect the best

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @s: String to modify
+ *
+ * Return: Pointer to the modified string
+ */
+
+char *cap_string(char *s)
+{
+	char *p = s;
+
+	while (*p)
+	{
+	if (*p >= 'a' && *p <= 'z')
+	{
+		if (p == s || *(p - 1) == ' ' || *(p - 1) == '\t' ||
+		*(p - 1) == '\n' || *(p - 1) == ',' || *(p - 1) == ';' ||
+		*(p - 1) == '.' || *(p - 1) == '!' || *(p - 1) == '?' ||
+		*(p - 1) == '"' || *(p - 1) == '(' || *(p - 1) == ')' ||
+		*(p - 1) == '{' || *(p - 1) == '}')
+			{
+				*p -= 32;
+			}
+	}
+		p++;
+	}
+
+	return (s);
+}
