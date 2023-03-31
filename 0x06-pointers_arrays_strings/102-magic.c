3933959492a43f8eb484ae8c0a372e2a168f4497
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 11:16:29 -0700

    A dream

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the third element of an array
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a[5] = {98, 402, 198, 1024, 3072};
+	int *p;
+
+	p = &a[2];
+	printf("a[2] = %d\n", *(p)); /* Prints the third element of the array */
+
+	return (0);
+}
