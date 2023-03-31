41aa5ae0ae527ae1e8b18e453c788c5b830bfe6b
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 07:12:57 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+/**
+ * _strncat - Concatenates two strings, using at most n bytes from src
+ * @dest: Destination string to append to
+ * @src: Source string to append from
+ * @n: Maximum number of bytes to use from src
+ * Return: Pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	/* Move the pointer to the end of the destination string */
+	while (*ptr != '\0')
+	{
+	ptr++;
+	}
+
+	/* Append the source string to the destination string, up to n bytes */
+	while (*src != '\0' && n > 0)
+	{
+	*ptr = *src;
+	ptr++;
+	src++;
+	n--;
+	}
+
+	/* Add the terminating null byte */
+	*ptr = '\0';
+
+	/* Return the pointer to the resulting string */
+	return dest;
+}
