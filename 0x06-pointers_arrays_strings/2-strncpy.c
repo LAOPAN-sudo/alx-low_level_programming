88dac23eb6637fd71e3c97b33c444f63799dafa6
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 07:43:12 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - Copies at most n bytes of src to dest, including the null byte
+ * @dest: Destination string to copy to
+ * @src: Source string to copy from
+ * @n: Maximum number of bytes to copy
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, const char *src, int n)
+{
+	char *dest_ptr = dest;
+	const char *src_ptr = src;
+
+	while (n-- > 0 && *src_ptr)
+	{
+	*dest_ptr++ = *src_ptr++;
+	}
+
+	while (n-- > 0)
+	{
+	*dest_ptr++ = '\0';
+	}
+
+	return dest;
+}
