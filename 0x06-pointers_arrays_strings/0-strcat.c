943622ca18a24a5ce6776f4cb011a15c69bc2e26
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 06:55:11 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,31 @@
 /**
  * _strcat - Concatenates two strings
- *
  * @dest: Destination string to append to
  * @src: Source string to append from
- *
  * Return: Pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-    char *ptr = dest;
+	char *ptr = dest;
 
-    /* Move the pointer to the end of the destination string */
-    while (*ptr != '\0')
-    {
-        ptr++;
-    }
+	/* Move the pointer to the end of the destination string */
+	while (*ptr != '\0')
+	{
+	ptr++;
+	}
 
-    /* Append the source string to the destination string */
-    while (*src != '\0')
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
+	/* Append the source string to the destination string */
+	while (*src != '\0')
+	{
+	*ptr = *src;
+	ptr++;
+	src++;
+	}
 
-    /* Add the terminating null byte */
-    *ptr = '\0';
+	/* Add the terminating null byte */
+	*ptr = '\0';
 
-    /* Return the pointer to the resulting string */
-    return dest;
+	/* Return the pointer to the resulting string */
+	return (dest);
 }
