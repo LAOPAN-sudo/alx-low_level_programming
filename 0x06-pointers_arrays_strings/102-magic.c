8436bbb9866019f9729711a5fb087f3de743dd71
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 11:42:31 -0700

    A dream

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  **/
+
 int main(void)
 {
 	int n;
@@ -13,10 +14,8 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
 	*(p + 5) = 98;
 
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
