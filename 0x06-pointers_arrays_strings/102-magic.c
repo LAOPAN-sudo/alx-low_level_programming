9c7bdedb2c7571bede1588bd6b00045c0e5b24ca
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 11:29:12 -0700

    A dream

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int a[5] = {98, 402, 198, 1024, 3072};
+	int a[5];
 	int *p;
 
 	p = &a[2];
 	*(p + 1) = 98;
-	printf("a[2] = %d\n", a[2]); /* Prints the third element of the array */
+	printf("a[2] = %d\n", *(p)); /* Prints the third element of the array */
 
 	return (0);
 }
