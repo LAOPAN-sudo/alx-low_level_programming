465d772bfabff86fc3a6fe8f9cf2faeb171e1718
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 16:02:44 -0700

    swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * reset_to_98 - a function that swaps the values of two integers
+ * @a: pointer to the integer 
+ * @b: pointer to the integer 
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int n
+	n = *a;
+	*a = *b;
+	*b = n;
+}
