9341430f651dd6017d1d2f7d3713bee3f6400e4c
Laopan-sudo <laopanpare@proonmail.com>
2023-03-31 17:18:45 -0700

    I can only go one way.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
