0b3ca6c0301313b00053a8beba7104937f3d46bf
Laopan-sudo <laopanpare@proonmail.com>
2023-04-06 14:42:21 -0700

    They say we only use a fraction

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,15 +12,31 @@ int is_palindrome(char *s)
 
 	if (len <= 1)
 		return (1);
-	if (*s != *(s + len - 1))
+	else
+		return (check_palindrome(s, 0, len - 1));
+}
+
+/**
+ * check_palindrome - helper function to check if a substring is a palindrome
+ * @s: the string to check
+ * @start: the index of the first character to check
+ * @end: the index of the last character to check
+ *
+ * Return: 1 if the substring is a palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] != s[end])
 		return (0);
-	*(s + len - 1) = '\0';
-	return (is_palindrome(s + 1));
+	else
+		return (check_palindrome(s, start + 1, end - 1));
 }
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: the string to measure
+ * @s: the string to check
  *
  * Return: the length of the string
  */
@@ -28,5 +44,6 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
