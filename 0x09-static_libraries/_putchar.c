92a77b94bb78a716078a96091cb9a3055535a1b7
Laopan-sudo <laopanpare@proonmail.com>
2023-04-10 13:09:22 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -6,7 +11,7 @@
  * Return: Always 0 (success)
  */
 
-int main(void)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
