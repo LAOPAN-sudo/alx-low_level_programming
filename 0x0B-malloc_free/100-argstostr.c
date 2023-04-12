833f887f5add8958c81443e37f83c4be822f6122
Laopan-sudo <laopanpare@proonmail.com>
2023-04-11 19:03:32 -0700

    5. It isnt the mountains ahead to climb that wear you out; its the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: pointer to the new string, or NULL on failure
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			str[k++] = av[i][j];
+
+		str[k++] = '\n';
+	}
+
+	str[k] = '\0';
+
+	return (str);
+}
