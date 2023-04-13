c5efced085865c3066b9d7890f249229f7269ac1
Laopan-sudo <laopanpare@proonmail.com>
2023-04-12 21:40:11 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,16 +3,40 @@
 #include "main.h"
 
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
- * @argc: argument count
- * @argv: argument vector
+ * calculateCoins - Calculates the minimum number of coins required to make
+ * change for an amount of money.
  *
- * Return: 0 on success, 1 on error
+ * @amount: The amount of money to make change for.
+ * @coin: The value of the coin to be used for making change.
+ *
+ * Return: The number of coins required to make change for the given amount.
+ */
+int calculateCoins(int amount, int coin)
+{
+	int numCoins = 0;
+
+	while (amount >= coin)
+	{
+		numCoins++;
+		amount -= coin;
+	}
+
+	return (numCoins);
+}
+
+/**
+ * main - Entry point for the program that calculates the minimum number of
+ * coins required to make change for an amount of money.
+ *
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the command line arguments.
+ *
+ * Return: 0 if the program completes successfully, otherwise 1.
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int coins = 0;
+	int amount;
 
 	if (argc != 2)
 	{
@@ -20,44 +44,21 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
+	amount = atoi(argv[1]);
 
-	if (cents < 0)
+	if (amount < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	while (cents >= 25)
-	{
-		coins++;
-		cents -= 25;
-	}
-
-	while (cents >= 10)
-	{
-		coins++;
-		cents -= 10;
-	}
-
-	while (cents >= 5)
-	{
-		coins++;
-		cents -= 5;
-	}
-
-	while (cents >= 2)
-	{
-		coins++;
-		cents -= 2;
-	}
-
-	while (cents >= 1)
-	{
-		coins++;
-		cents--;
-	}
+	coins += calculateCoins(amount, 25);
+	coins += calculateCoins(amount, 10);
+	coins += calculateCoins(amount, 5);
+	coins += calculateCoins(amount, 2);
+	coins += calculateCoins(amount, 1);
 
 	printf("%d\n", coins);
+
 	return (0);
 }
