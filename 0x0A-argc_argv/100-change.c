4d42631ffb56e1f11a47a31d3b7e91fd59ecd926
Laopan-sudo <laopanpare@proonmail.com>
2023-04-12 21:47:23 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,28 +3,6 @@
 #include "main.h"
 
 /**
- * calculateCoins - Calculates the minimum number of coins required to make
- * change for an amount of money.
- *
- * @amount: The amount of money to make change for.
- * @coin: The value of the coin to be used for making change.
- *
- * Return: The number of coins required to make change for the given amount.
- */
-int calculateCoins(int amount, int coin)
-{
-	int numCoins = 0;
-
-	while (amount >= coin)
-	{
-		numCoins++;
-		amount -= coin;
-	}
-
-	return (numCoins);
-}
-
-/**
  * main - Entry point for the program that calculates the minimum number of
  * coins required to make change for an amount of money.
  *
@@ -35,28 +13,35 @@ int calculateCoins(int amount, int coin)
  */
 int main(int argc, char *argv[])
 {
-	int coins = 0;
-	int amount;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	amount = atoi(argv[1]);
+	int cents = atoi(argv[1]);
 
-	if (amount < 0)
+	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	coins += calculateCoins(amount, 25);
-	coins += calculateCoins(amount, 10);
-	coins += calculateCoins(amount, 5);
-	coins += calculateCoins(amount, 2);
-	coins += calculateCoins(amount, 1);
+	int coins = 0;
+
+	coins += cents / 25;
+	cents = cents % 25;
+
+	coins += cents / 10;
+	cents = cents % 10;
+
+	coins += cents / 5;
+	cents = cents % 5;
+
+	coins += cents / 2;
+	cents = cents % 2;
+
+	coins += cents;
 
 	printf("%d\n", coins);
 
