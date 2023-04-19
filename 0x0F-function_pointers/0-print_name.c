1dd4b7e02cc784101765c8d9c0eee81ec3446f98
Laopan-sudo <laopanpare@proonmail.com>
2023-04-19 05:56:21 -0700

    0. Whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - Prints a name
+ * @name: Pointer to the name to be printed
+ * @f: Pointer to the function that prints the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
