de725847c5b4673c6f73b5163782101a93a23958
Laopan-sudo <laopanpare@proonmail.com>
2023-04-19 03:22:38 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,11 @@
 #define DOG_H
 
 /**
-
-struct dog - struct that defines a dog
-@name: pointer to a character string containing the dog's name
-@age: float representing the dog's age
-@owner: pointer to a character string containing the owner's name
-Description: struct dog contains information about a dog, including name,
-age, and owner.
+*struct dog - struct that defines a dog
+*@name: pointer to a character string containing the dog's name
+*@age: float representing the dog's age
+*@owner: pointer to a character string containing the owner's name
+*Description: struct dog contains information about a dog,name,age, and owner.
 */
 struct dog
 {
