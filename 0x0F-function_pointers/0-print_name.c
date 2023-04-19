eb5a0af59ba6afdc71b74d9f7ea5d49fda8b032d
Laopan-sudo <laopanpare@proonmail.com>
2023-04-19 06:27:57 -0700

    0. Whats my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  * print_name - Prints a name
@@ -9,8 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+	if (name && f)
+		f(name);
 }
