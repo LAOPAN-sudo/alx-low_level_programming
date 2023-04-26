7b5a85633311d423eb15ae3d84cc4f6a5a12c1c6
LAOPAN-sudo <laopanpare@protonmail.com>
2023-04-26 15:33:03 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -10,25 +12,15 @@
  * Return: index of the first int element || -1
  */
 
-/*
- * Author: Silas Mugambi
- * Description: This file defines the function int_index, which searches for an
- *        integer in an array of integers using
- *              a comparison function passed as an argument.
- */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
+	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	for (index = 0; index < size; index++)
+	if (size > 0 && array != NULL && cmp != NULL)
 	{
-		if (cmp(array[index]) != 0)
-			return (index);
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]) != 0)
+				return (i);
 	}
-
 	return (-1);
 }
