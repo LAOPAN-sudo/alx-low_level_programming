4854980fab14b5af8622eeebc7dd0ef6862db975
LAOPAN-sudo <laopanpare@protonmail.com>
2023-04-27 15:54:59 -0700

    6. Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 10, 0  ; hello string with newline character
-
-section .text
-    global _start
-
-_start:
-    ; prepare arguments to printf function
-    mov edi, hello   ; format string
-    xor eax, eax     ; zero out eax register (number of SSE registers used for arguments)
-    call printf      ; call printf function
-
-    ; exit program with return value of 0
-    xor edi, edi     ; return value 0
-    mov eax, 60      ; exit syscall number
-    syscall          ; call exit syscall
-
+ global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
