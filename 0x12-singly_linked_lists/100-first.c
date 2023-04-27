c0a6a0e9ce5580197e0d702b4c4c9be753252ebc
LAOPAN-sudo <laopanpare@protonmail.com>
2023-04-27 15:47:11 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,11 @@
 void startup_func(void) __attribute__ ((constructor));
 
 /**
- * startup_func - prints message before main
+ * startup_func - prints a message before the main function is executed
  */
 void startup_func(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
+
