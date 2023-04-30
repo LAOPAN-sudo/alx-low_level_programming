e633debe2fcb9c4a1ef433be97ce99b4b8a3990b
LAOPAN-sudo <laopanpare@protonmail.com>
2023-04-30 14:32:37 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,35 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		/* print value of current node */
-		int num = h->n;
-		int digits = 0;
-
-		if (num == 0)
-		{
-			_putchar('0');
-			digits++;
-		}
-		else if (num < 0)
-		{
-			_putchar('-');
-			num = -num;
-			digits++;
-		}
-		while (num != 0)
-		{
-			int digit = num % 10;
-
-			_putchar(digit + '0');
-			num /= 10;
-			digits++;
-		}
-		/* move to next node */
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
-		/* print newline if not at end of list */
-		if (h != NULL)
-			_putchar('\n');
 	}
 
 	return (count);
