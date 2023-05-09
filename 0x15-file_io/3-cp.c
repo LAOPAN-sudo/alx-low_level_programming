9c1cf01d0058a644ac2dd142f8a4b77993ba74ea
LAOPAN-sudo <laopanpare@protonmail.com>
2023-05-09 16:47:53 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,77 +1,88 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void copy_file(char *file_from, char *file_to);
 
 /**
  * copy_file - Copies the content of one file to another.
- * @file_from: The source file descriptor.
- * @file_to: The destination file descriptor.
+ * @file_from: The source file name.
+ * @file_to: The destination file name.
  */
-void copy_file(int file_from, int file_to)
+void copy_file(char *file_from, char *file_to)
 {
-	int bytes_read, bytes_written;
+	int from, to, r, w;
 	char buffer[1024];
 
-	while ((bytes_read = read(file_from, buffer, sizeof(buffer))) > 0)
+	from = open(file_from, O_RDONLY);
+	if (from == -1)
 	{
-		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to file descriptor %d\n", file_to);
-			exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 
-	if (bytes_read == -1)
+	to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	do {
+	r = read(from, buffer, sizeof(buffer));
+	if (r == -1)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't read from file descriptor %d\n", file_from);
+			"Error: Can't read from file %s\n", file_from);
 		exit(98);
+	}
+
+		w = write(to, buffer, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to file %s\n", file_to);
+			exit(99);
+		}
+
+	} while (r > 0);
+
+	if (close(from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", file_from);
+		exit(100);
+	}
+
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", file_to);
+		exit(100);
 	}
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of strings containing the arguments.
- * Return: 0 on success.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: On success, 0. On failure, an exit code.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	copy_file(file_from, file_to);
-
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", file_from);
-		exit(100);
-	}
-
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", file_to);
-		exit(100);
-	}
+	copy_file(argv[1], argv[2]);
 
 	return (0);
 }
